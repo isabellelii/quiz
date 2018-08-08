de687db3e2d2e1f77c3f45b89dfa11e70b09eba1
Isabelle <isabellelidberg@live.se>
2018-08-08 16:09:48 +0200

    adds submit button

--- quiz.js
+++ quiz.js
@@ -1,2 +1,14 @@
 alert('Hey you, ready for a quiz?');
 console.log(alert);
+
+const quizContainer = document.getElementById('quiz');
+const resultsContainer = document.getElementById('results');
+const submitButton = document.getElementById('submit');
+
+function quizContainer() {};
+
+function resultsContainer(){};
+
+buildQuiz();
+
+submitButton.addEventListener('click', showResults);
